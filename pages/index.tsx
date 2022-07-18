74807986a5681b607f592fb05152bfa5f90f4f7b
erobert <etiennerobert33@gmail.com>
2022-07-18 22:34:16 +0200

    Add TaskGraph

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import BusinessCard from "../components/BusinessCard";
 import Project from "../components/Project";
 
+import styles from "./index.module.css";
+
 const App = () => (
   <>
     <BusinessCard />
-    <Project
-      name="Minitick"
-      url="https://etrobert.github.io/minitick/"
-      image="images/minitick.png"
-    />
+    <div className={styles.projects}>
+      <Project
+        name="Minitick"
+        url="https://etrobert.github.io/minitick/"
+        image="images/minitick.png"
+      />
+      <Project
+        name="TaskGraph"
+        url="https://taskgraph.net"
+        image="images/taskgraph.png"
+      />
+    </div>
   </>
 );
 
