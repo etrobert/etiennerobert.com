ba2b3c1f742abecc3acd3314a3e994063cc3577e
etrobert <etiennerobert33@gmail.com>
2023-05-09 19:41:32 +0000

    Deploying to gh-pages from @ etrobert/etiennerobert.com@8a117a052d114f145f8be13bde8c49797f3e0ed1 🚀

---
+++ _next/static/D9RwKVEq833Xm-mJF-cOA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8e1c8b2bea6b7a5d.css","static/chunks/pages/index-e80dd4f18513337a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
