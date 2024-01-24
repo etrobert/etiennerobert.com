70d4f3af0b3010403378ea77fbcde8ee83962694
erobert <etiennerobert33@gmail.com>
2024-01-24 18:27:05 +0100

    Simplify call and extract message content

--- components/assistantCall.ts
+++ components/assistantCall.ts
@@ -5,37 +5,11 @@ import OpenAI from 'openai';
 const openai = new OpenAI();
 
 async function assistantCall() {
-  const messages = [
-    { role: 'user', content: "What's the weather like in Boston today?" },
-  ];
-  const tools = [
-    {
-      type: 'function',
-      function: {
-        name: 'get_current_weather',
-        description: 'Get the current weather in a given location',
-        parameters: {
-          type: 'object',
-          properties: {
-            location: {
-              type: 'string',
-              description: 'The city and state, e.g. San Francisco, CA',
-            },
-            unit: { type: 'string', enum: ['celsius', 'fahrenheit'] },
-          },
-          required: ['location'],
-        },
-      },
-    },
-  ];
-
-  // @ts-expect-error Code comes from documentation
-  return await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages: messages,
-    tools: tools,
-    tool_choice: 'auto',
+  const result = await openai.chat.completions.create({
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+    model: 'gpt-4',
   });
+  return result.choices[0].message.content;
 }
 
 export { assistantCall };
