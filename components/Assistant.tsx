45c38f400b464c646efa5e51b247c7dc10d299b8
erobert <etiennerobert33@gmail.com>
2024-01-30 11:18:35 +0100

    Extract AssistantMessages

--- components/Assistant.tsx
+++ components/Assistant.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import AssistantInput from './AssistantInput';
 import { assistantCall } from './assistantCall';
 import style from './Assistant.module.scss';
@@ -8,10 +8,9 @@ import style from './Assistant.module.scss';
 import type { Message } from './assistantCall';
 
 import system from './system';
+import AssistantMessages from './AssistantMessages';
 
 const Assistant = () => {
-  const singletonRef = useRef<HTMLDivElement>(null);
-
   const [messages, setMessages] = useState<Message[]>([
     { role: 'system', content: system },
   ]);
@@ -27,28 +26,11 @@ const Assistant = () => {
     setMessages((messages) => [...messages, result]);
   };
 
-  useEffect(() => {
-    if (messages.length === 1) return; // Avoid scrolling on first render
-    singletonRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [messages]);
-
   return (
     // TODO: Remove outerContainer that exists only for style
     <div className={style.outerContainer}>
       <div className={style.container}>
-        <ol className={style.messages}>
-          {messages
-            .filter(({ role }) => role !== 'system')
-            .map((message, index) => (
-              <li key={index}>
-                {message.role === 'user' ? 'You' : 'Assistant'}
-                <br /> {/* TODO Remove br */}
-                {message.content}
-              </li>
-            ))}
-          {loadingState === 'loading' && 'loading...'}
-          <div ref={singletonRef} />
-        </ol>
+        <AssistantMessages messages={messages} loadingState={loadingState} />
         <AssistantInput
           onInput={(message) => {
             const newMessages = [
