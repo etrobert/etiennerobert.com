95aab098ad17e52129352fea0dce415452c86f16
Etienne Robert <etiennerobert33@gmail.com>
2024-03-04 12:01:19 +0100

    Minor design tweaks

--- components/BusinessCardText.tsx
+++ components/BusinessCardText.tsx
@@ -7,12 +7,10 @@ import IconLink from './IconLink';
 
 const BusinessCardText = () => (
   <div className="text-[1.5rem] lg:text-[2rem]">
-    <h1
-      className={`text-[2.1em] tracking-tight leading-none ${title.className}`}
-    >
+    <h1 className={`text-[2em] tracking-tight leading-none ${title.className}`}>
       Étienne Robert
     </h1>
-    <JustifiedText className={'text-[0.83em]'}>
+    <JustifiedText className={'text-[0.79em] leading-tight'}>
       Software Developer & Queer Creative
     </JustifiedText>
     <div className={'flex justify-center gap-[5vw]'}>
