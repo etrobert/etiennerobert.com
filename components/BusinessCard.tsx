d71cd2b91d264cb2ad297c002e0fe943a058d988
Etienne Robert <etiennerobert33@gmail.com>
2024-03-04 11:44:39 +0100

    Extract BusinessCardText to isolate font-size magic

--- components/BusinessCard.tsx
+++ components/BusinessCard.tsx
@@ -1,40 +1,15 @@
-import GithubIcon from './icons/GithubIcon';
-import LinkedinIcon from './icons/LinkedinIcon';
-import InstagramIcon from './icons/InstagramIcon';
-import { title } from '../app/fonts';
-import JustifiedText from './JustifiedText';
-import IconLink from './IconLink';
+import BusinessCardText from './BusinessCardText';
 import Portrait from './Portrait';
 
 const BusinessCard = () => (
   <div
     className={
       'flex flex-col place-content-center place-items-center h-full ' +
-      'text-[1.5rem] lg:text-[2rem] lg:flex-row-reverse lg:gap-24'
+      'lg:flex-row-reverse lg:gap-24'
     }
   >
     <Portrait />
-    <div>
-      <h1
-        className={`text-[2.1em] tracking-tight leading-none ${title.className}`}
-      >
-        Étienne Robert
-      </h1>
-      <JustifiedText className={'text-[0.83em]'}>
-        Software Developer & Queer Creative
-      </JustifiedText>
-      <div className={'flex justify-center gap-[5vw]'}>
-        <IconLink href="https://github.com/etrobert" icon={<GithubIcon />} />
-        <IconLink
-          href="https://www.linkedin.com/in/etienne-robert-dev/"
-          icon={<LinkedinIcon />}
-        />
-        <IconLink
-          href="https://www.instagram.com/etrobert_"
-          icon={<InstagramIcon />}
-        />
-      </div>
-    </div>
+    <BusinessCardText />
   </div>
 );
 
---
+++ components/BusinessCardText.tsx
@@ -0,0 +1,32 @@
+import GithubIcon from './icons/GithubIcon';
+import LinkedinIcon from './icons/LinkedinIcon';
+import InstagramIcon from './icons/InstagramIcon';
+import { title } from '../app/fonts';
+import JustifiedText from './JustifiedText';
+import IconLink from './IconLink';
+
+const BusinessCardText = () => (
+  <div className="text-[1.5rem] lg:text-[2rem]">
+    <h1
+      className={`text-[2.1em] tracking-tight leading-none ${title.className}`}
+    >
+      Étienne Robert
+    </h1>
+    <JustifiedText className={'text-[0.83em]'}>
+      Software Developer & Queer Creative
+    </JustifiedText>
+    <div className={'flex justify-center gap-[5vw]'}>
+      <IconLink href="https://github.com/etrobert" icon={<GithubIcon />} />
+      <IconLink
+        href="https://www.linkedin.com/in/etienne-robert-dev/"
+        icon={<LinkedinIcon />}
+      />
+      <IconLink
+        href="https://www.instagram.com/etrobert_"
+        icon={<InstagramIcon />}
+      />
+    </div>
+  </div>
+);
+
+export default BusinessCardText;
