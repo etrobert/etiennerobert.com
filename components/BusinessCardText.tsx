fda109d78bc778f113c963897ef1d1fccc8a5e96
Etienne Robert <etiennerobert33@gmail.com>
2024-03-04 12:05:18 +0100

    Extract Links component

--- components/BusinessCardText.tsx
+++ components/BusinessCardText.tsx
@@ -1,9 +1,6 @@
-import GithubIcon from './icons/GithubIcon';
-import LinkedinIcon from './icons/LinkedinIcon';
-import InstagramIcon from './icons/InstagramIcon';
 import { title } from '../app/fonts';
 import JustifiedText from './JustifiedText';
-import IconLink from './IconLink';
+import Links from './Links';
 
 const BusinessCardText = () => (
   <div className="text-[1.5rem] lg:text-[2rem]">
@@ -13,17 +10,7 @@ const BusinessCardText = () => (
     <JustifiedText className={'text-[0.79em] leading-tight'}>
       Software Developer & Queer Creative
     </JustifiedText>
-    <div className={'flex justify-center gap-[5vw]'}>
-      <IconLink href="https://github.com/etrobert" icon={<GithubIcon />} />
-      <IconLink
-        href="https://www.linkedin.com/in/etienne-robert-dev/"
-        icon={<LinkedinIcon />}
-      />
-      <IconLink
-        href="https://www.instagram.com/etrobert_"
-        icon={<InstagramIcon />}
-      />
-    </div>
+    <Links />
   </div>
 );
 
